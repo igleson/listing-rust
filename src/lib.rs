aa45b3ebb9a85d63df890d2ec62cb4fed242c29b
igleson <igleson.freire@vtex.com.br>
2025-02-21 17:42:24 -0300

    http example

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,9 @@
-use axum::{routing::get, Router};
-use tower_service::Service;
 use worker::*;
 
-fn router() -> Router {
-    Router::new().route("/", get(root))
-}
-
 #[event(fetch)]
-async fn fetch(
-    req: HttpRequest,
-    _env: Env,
-    _ctx: Context,
-) -> Result<axum::http::Response<axum::body::Body>> {
+async fn fetch(_req: HttpRequest, _env: Env, _ctx: Context) -> Result<HttpResponse> {
     console_error_panic_hook::set_once();
-    Ok(router().call(req).await?)
-}
-
-pub async fn root() -> &'static str {
-    "Hello Axum!"
+    Ok(http::Response::builder()
+        .status(http::StatusCode::OK)
+        .body(Body::empty())?)
 }
